94599dd9d9d62f4545b2e84d7d005f50590ad94e
VitorORalves <voractl@gmail.com>
2023-09-28 14:16:25 -0300

    user update with PUT

--- src/main/java/com/example/workshopmongo/services/UserService.java
+++ src/main/java/com/example/workshopmongo/services/UserService.java
@@ -35,7 +35,19 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		Optional<User> newObj = repo.findById(obj.getId());
+		updateData(newObj.orElseThrow(() -> new ObjectNotFoundException("Object not found")), obj);
+		return repo.save(newObj.get());
+	}
+
+	public void updateData(User newObj, User obj){
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
+	
 }
